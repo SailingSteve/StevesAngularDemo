a8176b0ebf7d0ded5c5cf2dadbffaebe55dfd562
stevepodell <steve@podell.com>
2014-05-14 15:36:40 -0700

    Cleaned up styling.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 (function () {
 
     // Define it here, use it elsewhere without the [] which would redefine it!
-    angular.module('angularCustApp',[]);
+    angular.module('angularCustApp', []);
 
 }());
--- app/controllers/CustomersController.js
+++ app/controllers/CustomersController.js
@@ -1,15 +1,15 @@
 (function () {
     angular.module('angularCustApp').factory('instagram', ['$http',
-        function($http){
+        function ($http) {
             return {
-                fetchPopular: function(callback){
+                fetchPopular: function (callback) {
                     var endPoint = "https://api.instagram.com/v1/media/popular?client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK";
 
-                    $http.jsonp(endPoint).success(function(response){
+                    $http.jsonp(endPoint).success(function (response) {
                         callback(response.data);
                     });
                 }
-	       }
+            };
         }]);
 
     var CustomersController =
@@ -17,12 +17,13 @@
             $scope.sortBy = 'name';
             $scope.reverse = false;
             $scope.pics = [];
+            var i;
 
             $scope.customers = [
-                {joined: '1965-25-1', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99, pic: ''},
-                {joined: '2014-1-1', name: 'Dave', city: 'Camarillo', orderTotal: 9.999534, pic: ''},
-                {joined: '2014-2-2', name: 'Drew', city: 'LA', orderTotal: 9.01, pic: ''},
-                {joined: '2014-3-1', name: 'Jennifer', city: 'Napa', orderTotal: 9.01, pic: '' } ];
+                {joined: '1965-25-01', name: 'Zed', city: 'Las Vegas', orderTotal: 19.99, pic: ''},
+                {joined: '2014-01-01', name: 'Dave', city: 'Camarillo', orderTotal: 9.999534, pic: ''},
+                {joined: '2014-02-02', name: 'Drew', city: 'LA', orderTotal: 9.01, pic: ''},
+                {joined: '2014-03-01', name: 'Jennifer', city: 'Napa', orderTotal: 9.01, pic: '' } ];
 
             $scope.doSort = function (propName) {
                 $scope.sortBy = propName;
@@ -35,19 +36,16 @@
             $scope.refreshImages = function () {
                 instagram.fetchPopular(function (data) {
                     $scope.pics = data;
-                    for( i = 0; i < $scope.customers.length; i++ ) {
+
+                    for (i = 0; i < $scope.customers.length; i++) {
                         $scope.customers[i].pic = data[i];
                     }
                 });
             };
 
             $scope.refreshImages();
-        }
+        };
 
     angular.module('angularCustApp').controller('CustomersController', CustomersController);
 
 }());
-
-
-
-
